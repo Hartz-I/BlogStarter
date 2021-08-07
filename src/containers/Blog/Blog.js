cb512d29b725732a7dc9524c8f361daab7a7aa96
Ahnaf Sadik Inan <62875018+Hartz-I@users.noreply.github.com>
2021-08-08 03:40:53 +0600

    shortened data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -15,15 +15,23 @@ class Blog extends Component {
       .get("https://jsonplaceholder.typicode.com/posts")
       //then executes after the request is finished
       .then((resnponse) => {
+        const posts = resnponse.data.slice(0, 4); //to get relive from too many data
+        const updatedPosts = posts.map((post) => {
+          return {
+            ...post,
+            author: "Max", //can work with the data we get back
+          };
+        });
+
         //set state won't work outside cuz JS doesn't work while the data is fetched
-        this.setState({ posts: resnponse.data });
+        this.setState({ posts: updatedPosts });
         console.log(resnponse); ///see what's inside
       });
   }
 
   render() {
     const posts = this.state.posts.map((post) => {
-      return <Post title={post.title} key={post.id} />;
+      return <Post title={post.title} key={post.id} author={post.author} />;
     });
     return (
       <div>
