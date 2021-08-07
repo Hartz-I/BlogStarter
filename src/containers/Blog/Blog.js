f6f02596cc6c444e9736c86c134d767e5b05d168
Ahnaf Sadik Inan <62875018+Hartz-I@users.noreply.github.com>
2021-08-08 03:33:14 +0600

    added data from server to Blog.js file

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import React, { Component } from "react";
+import axios from "axios";
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      //then executes after the request is finished
+      .then((resnponse) => {
+        //set state won't work outside cuz JS doesn't work while the data is fetched
+        this.setState({ posts: resnponse.data });
+        console.log(resnponse); ///see what's inside
+      });
+  }
+
+  render() {
+    const posts = this.state.posts.map((post) => {
+      return <Post title={post.title} key={post.id} />;
+    });
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
