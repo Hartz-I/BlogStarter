6db74ab31daef979d6a99d38eddb9b07762b15b5
Ahnaf Sadik Inan <62875018+Hartz-I@users.noreply.github.com>
2021-08-15 16:38:25 +0600

    notes on passing param and switch

---
+++ bricks/6.TillPassingParamAndSwitch/src/containers/Blog/Blog.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+// import Post from "../../components/Post/Post";
+// import FullPost from "../FullPost/FullPost";
+// import NewPost from "../../components/NewPost/NewPost";
+
+//use links instead of A for routing links
+import { Route, NavLink, Switch } from "react-router-dom";
+//Link doesn't have active class but NavLink does
+
+import "./Blog.css";
+
+import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
+import FullPost from "./FullPost/FullPost";
+
+// routing to index.js or app.js
+//full post and new post can be containers cuz now they will be new pages each with own state
+
+class Blog extends Component {
+  render() {
+    return (
+      <div className="Blog">
+        {null /* nav links */}
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink
+                  to="/"
+                  exact /** need exact to be active only when is clicked */
+                  //activeClassName to set active class manually
+                  //activeStyle property to inline style
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={
+                    //now page won't reload. just content will
+                    {
+                      //use dif property
+                      pathname: "/new-post", //always gives a absolute path
+                      //pathname: this.props.match.url + "./new-post", //gives a relative path
+                      hash: "#submit",
+                      search: "/quick-submit=true",
+                    }
+                  }
+                >
+                  New Post
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {/* <Route
+          //if multiple route has same path 2nd one will show belo first one
+          path="/"
+          render={() => <h1>Home1</h1>}
+        />
+        <Route
+          //if multiple route has same path 2nd one will show belo first one
+          path="/"
+          exact={true /**only for / not new-post }
+          render={() => <h1>Home2</h1>}
+        /> */}
+        <Switch /** it on let's to load one of them */>
+          <Route path="/" exact component={Posts} />
+          <Route
+            path="/new-post"
+            exact={true /**only for / not new-post */}
+            //render={() => <NewPost />} shouln't use render to load component
+            component={NewPost}
+          />
+          <Route
+            path="/:id" //this gives a flexible path //now for the error changing the name would work but we'll use a package
+            exact
+            component={FullPost} /*the link is in posts each post*/
+          />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default Blog;
---
+++ bricks/6.TillPassingParamAndSwitch/src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import axiosInstance from "../../../axiosInstance";
+
+import { Link } from "react-router-dom";
+
+import "./Posts.css";
+import Post from "../../../components/Post/Post";
+
+class Posts extends Component {
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {
+    console.log(this.props);
+
+    axiosInstance
+      .get("/posts") //short cuz URL is set default
+      //then executes after the request is finished
+      .then((resnponse) => {
+        const posts = resnponse.data.slice(0, 4); //to get relive from too many data
+        const updatedPosts = posts.map((post) => {
+          return {
+            ...post,
+            author: "Ahnaf", //can work with the data we get back
+          };
+        });
+
+        //set state won't work outside cuz JS doesn't work while the data is fetched
+        this.setState({ posts: updatedPosts });
+        //console.log(resnponse); ///see what's inside
+      })
+      .catch((error) => {
+        //handles error locally
+        console.log(error);
+        //global handling is in index.js
+        //this.setState({ error: true });
+      });
+  }
+
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  };
+
+  render() {
+    //using catch error we can render when error is made!
+    let posts = <p>Something went wrong!!</p>;
+    //map what the posts are
+    if (!this.state.error) {
+      posts = this.state.posts.map((post) => {
+        return (
+          <Link to={"/" + post.id} key={post.id}>
+            <Post
+              title={post.title}
+              author={post.author}
+              clicked={() => {
+                this.postSelectedHandler(post.id);
+              }}
+            />
+          </Link>
+        );
+      });
+    }
+    return <section className="Posts">{posts}</section>;
+  }
+}
+
+export default Posts;
